d950f4d0bd0595a11e8e227aa377f0a0147ecb18
Johan Lindén <johan.linden@knowit.se>
2025-09-25 10:49:07 +0200

    add correct url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,17 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import axios from 'axios'  // Add this import
+import axios from 'axios'
 import App from './App.vue'
 import Login from './Login.vue'
 import Users from './Users.vue'
 import './assets/main.css' 
 
-
+// Set the API base URL to your container
 if (import.meta.env.PROD) {
-  axios.defaults.baseURL = import.meta.env.VITE_API_URL
+  axios.defaults.baseURL = 'http://userapi09041259.swedencentral.azurecontainer.io:8080'
 }
+
+console.log('API Base URL:', axios.defaults.baseURL)
 
 const routes = [
   { path: '/', redirect: '/users' },
